2359773f9e6db9142c2ab64f3b5ed78b01e584dc
Allan Irvine <alirvine@gmail.com>
2016-03-10 08:01:43 -0500

    Perfect forwarding

---
+++ AlMath/include/Jasper/CapsuleCollider.cpp
@@ -0,0 +1,34 @@
+#include <Jasper\CapsuleCollider.h>
+#include <Jasper\GameObject.h>
+#include <Jasper\Mesh.h>
+
+Jasper::CapsuleCollider::CapsuleCollider(const std::string & name, Mesh * mesh, PhysicsWorld * world)
+	: PhysicsCollider(name, mesh, world)
+{
+}
+
+Jasper::CapsuleCollider::~CapsuleCollider()
+{
+}
+
+void Jasper::CapsuleCollider::Awake()
+{
+	auto go = GetGameObject();
+	auto& trans = go->GetLocalTransform();
+	auto& btTrans = trans.GetBtTransform();
+
+	Vector3 extents = m_mesh->GetHalfExtents();
+	float radius = std::max(extents.x, extents.z);
+	float height = extents.y * 2.0f;
+
+	m_collisionShape = new btCapsuleShape(radius, height);
+
+	btVector3 inertia;
+	m_collisionShape->calculateLocalInertia(Mass, inertia);
+
+	m_defaultMotionState = new btDefaultMotionState(btTrans);
+	btRigidBody::btRigidBodyConstructionInfo rbci(Mass, m_defaultMotionState, m_collisionShape, inertia);
+	m_rigidBody = new btRigidBody(rbci);
+
+	m_world->AddRigidBody(m_rigidBody);
+}
