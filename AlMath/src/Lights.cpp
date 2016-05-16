0c43b5bd21ad07064db20d218840d366dd84c388
Allan Irvine <alirvine@gmail.com>
2016-05-16 17:50:32 -0400

    More files

---
+++ AlMath/src/Lights.cpp
@@ -0,0 +1,28 @@
+#include <Jasper\Lights.h>
+
+namespace Jasper {
+
+PointLight::PointLight(const std::string& name) 
+	: GameObject(name)
+{
+
+}
+
+
+PointLight::~PointLight()
+{
+}
+
+
+DirectionalLight::DirectionalLight(const std::string & name)
+	: GameObject(name)
+{
+}
+
+DirectionalLight::~DirectionalLight()
+{
+}
+
+
+
+}
